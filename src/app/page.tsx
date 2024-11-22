5b1ad90b7a8ddb9bab0552e8adfc65a4fc98b8cf
Jean Philippe VEIGNE <58737612+jveigne@users.noreply.github.com>
2024-11-23 00:57:45 +0100

    page accueil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,34 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-900 to-blue-600 text-white">
       {/* Titre principal */}
-      <h1 className="text-5xl font-extrabold mb-6 text-center">
-        Soyez les Bienvenue au <span className="text-yellow-400">ZTF Awards 2024</span>
-      </h1>
+        <h1 className="text-5xl font-extrabold mb-6 text-center">
+            Bienvenue aux <span className="text-yellow-400">ZTF Awards 2024</span>
+        </h1>
 
-      {/* Sous-titre */}
-      <p className="text-lg mb-10 text-center max-w-2xl">
-        {/*Célébrons ensemble les talents musicaux qui inspirent et touchent nos cœurs. 
+        <br/>
+
+        <h1 className="text-5xl font-extrabold mb-6 text-center">
+            Welcome to the <span className="text-yellow-400">ZTF Awards 2024</span>
+        </h1>
+        {/* Sous-titre */}
+        <p className="text-lg mb-10 text-center max-w-2xl">
+            {/*Célébrons ensemble les talents musicaux qui inspirent et touchent nos cœurs.
         Découvrez les meilleurs artistes, chansons et moments marquants de l'année.*/}
-      </p>
+        </p>
 
-      {/* Conteneur des boutons */}
-      <div className="flex flex-col items-center space-y-4">
-        {/* Boutons d'action */}
-        <button className="bg-yellow-400 text-blue-900 px-6 py-3 rounded-full font-semibold text-lg shadow hover:bg-yellow-500 w-64">
-          <Link href="/program">Programme du ZTF Awards</Link>
-        </button>
-        <button className="bg-white text-blue-900 px-6 py-6 rounded-full font-semibold text-lg shadow hover:bg-gray-200 w-64">
-  <Link href="/recueil">Recueil Musical</Link>
-</button>
+        {/* Conteneur des boutons */}
+        <div className="flex flex-col items-center space-y-4">
+            {/* Boutons d'action */}
+            <button
+                className="bg-yellow-400 text-blue-900 px-6 py-3 rounded-full font-semibold text-lg shadow hover:bg-yellow-500 w-64">
+                <Link href="/program">Programme du ZTF Awards</Link>
+            </button>
+            <button
+                className="bg-white text-blue-900 px-6 py-6 rounded-full font-semibold text-lg shadow hover:bg-gray-200 w-64">
+                <Link href="/recueil">Recueil Musical</Link>
+            </button>
 
-      </div>
+        </div>
     </div>
   );
 }
