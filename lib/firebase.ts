47b5f4cb5309f2a1a0ca847d34a7696c0e48e28e
Jean Philippe VEIGNE <58737612+jveigne@users.noreply.github.com>
2024-11-22 16:21:47 +0100

    add variable

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBSCiO-i8N8T00PGcBB6elXqTzA1pBsdHE",
-    authDomain: "receuilztfawards.firebaseapp.com",
-    projectId: "receuilztfawards",
-    storageBucket: "receuilztfawards.firebasestorage.app",
-    messagingSenderId: "903148873436",
-    appId: "1:903148873436:web:1764e5778dc5cd17faf619",
-    measurementId: "G-992S0PWNH9"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
