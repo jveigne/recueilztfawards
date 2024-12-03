d81f12b9d3bd847285d3cd4b4afb8188f2fb28ca
Jean Philippe VEIGNE <58737612+jveigne@users.noreply.github.com>
2024-12-03 22:49:58 +0100

    add next config

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    async headers() {
+        return [
+            {
+                // Appliquer à toutes les routes API
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // Remplacez '*' par votre domaine si nécessaire
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ],
+            },
+        ];
+    },
+};
